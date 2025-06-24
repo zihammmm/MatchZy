5f06b60de9611417d3144f3be8ec6ff166655257
NXYorz <niexiaoyang12138@126.com>
2025-06-25 02:10:05 +0800

    修正缩进

--- MatchManagement.cs
+++ MatchManagement.cs
@@ -540,18 +540,18 @@ namespace MatchZy
             var steamId = player.SteamID;
             
             Log($"[GetPlayerTeam] - DEBUG]: before try");
-	    try
+	        try
             {
                 if (matchzyTeam1.teamPlayers != null && matchzyTeam1.teamPlayers[steamId.ToString()] != null)
                 {
                     if (teamSides[matchzyTeam1] == "CT")
                     {
                     	Log($"[GetPlayerTeam] - DEBUG]: team1 , CT");
-		        playerTeam = CsTeam.CounterTerrorist;
+		                playerTeam = CsTeam.CounterTerrorist;
                     }
                     else if (teamSides[matchzyTeam1] == "TERRORIST")
                     {
-			Log($"[GetPlayerTeam] - DEBUG]: team1 , T");
+			            Log($"[GetPlayerTeam] - DEBUG]: team1 , T");
                         playerTeam = CsTeam.Terrorist;
                     }
 
@@ -560,19 +560,19 @@ namespace MatchZy
                 {
                     if (teamSides[matchzyTeam2] == "CT")
                     {
-			Log($"[GetPlayerTeam] - DEBUG]: team2 , CT");
+			            Log($"[GetPlayerTeam] - DEBUG]: team2 , CT");
                         playerTeam = CsTeam.CounterTerrorist;
                     }
                     else if (teamSides[matchzyTeam2] == "TERRORIST")
                     {
-			Log($"[GetPlayerTeam] - DEBUG]: team2 , T");
+			            Log($"[GetPlayerTeam] - DEBUG]: team2 , T");
                         playerTeam = CsTeam.Terrorist;
                     }
                 }
                 else if (matchConfig.Spectators != null && matchConfig.Spectators[steamId.ToString()] != null)
                 {
                     Log($"[GetPlayerTeam] - DEBUG]: SPectators");
-		    playerTeam = CsTeam.Spectator;
+		            playerTeam = CsTeam.Spectator;
                 }
             }
             catch (Exception ex)
@@ -587,35 +587,33 @@ namespace MatchZy
            CsTeam playerTeam = CsTeam.None;
            var steamId = player.SteamID;
            int remainingMaps = matchConfig.NumMaps - matchzyTeam1.seriesScore - matchzyTeam2.seriesScore;
-	   if (true)
+	        if (true)
            {
                 Log($"[forceChangeSide - FATAL]:matchConfig.Maplist.Count == 0");
                 Log($"[forceChangeSide - DEBUG]:matchConfig.CurrentMapNumber: { matchConfig.CurrentMapNumber}");
                 Log($"[forceChangeSide - DEBUG]: matchConfig.NumMaps: { matchConfig.NumMaps}");
-	   }
-//else
-           if(remainingMaps > 0)
- 	   {
-		Log($"[forceChangeSide - DEBUG]:remainingMaps:{remainingMaps}");
-		
-		int currentMapNumber = 3 - remainingMaps;
-
-		Log($"[forceChangeSide - DEBUG]:mapPlayerTeam[currentMapNumber][player.PlayerName]:{mapPlayerTeam[currentMapNumber][player.PlayerName]}");
-                Log($"[[forceChangeSide - DEBUG]:player.PlayerName:{player.PlayerName}");
-                if (mapPlayerTeam[currentMapNumber][player.PlayerName] == "CT")
-                { 
-                    player.ChangeTeam(CsTeam.CounterTerrorist);
-                }
-                else if (mapPlayerTeam[currentMapNumber][player.PlayerName] == "T")
-                {
-                    player.ChangeTeam(CsTeam.Terrorist);
-                }
-                else
-                {
-                    Log($"[forceChangeSide error]");
-                }
            }
-               
+           if(remainingMaps > 0)
+           {
+               Log($"[forceChangeSide - DEBUG]:remainingMaps:{remainingMaps}");
+		
+               int currentMapNumber = 3 - remainingMaps;
+
+               Log($"[forceChangeSide - DEBUG]:mapPlayerTeam[currentMapNumber][player.PlayerName]:{mapPlayerTeam[currentMapNumber][player.PlayerName]}");
+               Log($"[[forceChangeSide - DEBUG]:player.PlayerName:{player.PlayerName}");
+               if (mapPlayerTeam[currentMapNumber][player.PlayerName] == "CT")
+               { 
+                   player.ChangeTeam(CsTeam.CounterTerrorist);
+               }
+               else if (mapPlayerTeam[currentMapNumber][player.PlayerName] == "T")
+               {
+                   player.ChangeTeam(CsTeam.Terrorist);
+               }
+               else
+               {
+                   Log($"[forceChangeSide error]");
+               }
+           }
        }
         public void EndSeries(string? winnerName, int restartDelay, int t1score, int t2score)
         {
